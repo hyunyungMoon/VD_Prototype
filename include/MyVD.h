8f6ac1b2e8f2fe29f7e3c118a3f75f3bd2e97483
hyunyung <hymoon.vdrc@gmail.com>
2022-02-28 15:34:40 +0900

    adjusting vsCode

---
+++ include/MyVD.h
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+#include "BoundaryPolygon.h"
+#include "MyEdge.h"
+#include "MyVertex.h"
+#include "WingedEdgeDataStructure.h"
+#include "rg_Circle2D.h"
+#include "rg_Const.h"
+#include "rg_Point2D.h"
+
